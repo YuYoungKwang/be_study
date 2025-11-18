6a63a23cb6f07ffae84f8423004945e14da664ce
YuYoungKwang <db99817@gmail.com>
2025-11-18 15:24:11 +0900

    클래스 리턴타입 활용 예시

---
+++ src/be_study/cls/cls11/AllScore.java
@@ -0,0 +1,27 @@
+package be_study.cls.cls11;
+
+public class AllScore {
+	
+	public int getLang() {
+		return lang;
+	}
+	public void setLang(int lang) {
+		this.lang = lang;
+	}
+	public int getMath() {
+		return math;
+	}
+	public void setMath(int math) {
+		this.math = math;
+	}
+	public int getEng() {
+		return eng;
+	}
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+	
+	int lang;
+	int math;
+	int eng;
+}
