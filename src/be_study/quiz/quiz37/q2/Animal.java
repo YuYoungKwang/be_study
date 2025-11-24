96ef5ad09a9c27e39bc4c9c881e683d55349eec5
YuYoungKwang <db99817@gmail.com>
2025-11-24 16:03:58 +0900

    퀴즈37 클래스 상속

---
+++ src/be_study/quiz/quiz37/q2/Animal.java
@@ -0,0 +1,9 @@
+package be_study.quiz.quiz37.q2;
+
+class Animal {
+	String name; // 이름
+
+	public void cry() {
+		System.out.println("동물이 웁니다!");
+	}
+}
---
+++ src/be_study/quiz/quiz37/q2/Pet.java
@@ -0,0 +1,11 @@
+package be_study.quiz.quiz37.q2;
+
+public class Pet extends Animal {
+
+	int age; // 나이
+	int price; // 가격
+
+	public void play() {
+		System.out.println("애완 동물이 장난을 칩니다~");
+	}
+}
