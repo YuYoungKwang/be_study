f52bfdf325191247cf9f8cd6630b9bad1320cdf0
YuYoungKwang <db99817@gmail.com>
2026-01-20 09:29:03 +0900

    퀴즈 52 DB연동 Select 날짜

---
+++ src/be_study/quiz/quiz54/Quiz54.java
@@ -0,0 +1,25 @@
+package be_study.quiz.quiz54;
+
+import java.util.List;
+
+public class Quiz54 {
+
+	public static void main(String[] args) {
+
+		StudentDAO studentDAO = new StudentDAO();
+		
+		List<StudentDTO> list1 = studentDAO.findStudentList();
+		
+		for(StudentDTO s : list1) {
+			System.out.println(s);
+		}
+		
+		List<StudentDTO> list2 = studentDAO.findStudentListByGrade(3);
+		
+		System.out.println("grade 기준 3학년 데이터******************");
+		for(StudentDTO s : list2) {
+			System.out.println(s);
+		}
+	}
+
+}
