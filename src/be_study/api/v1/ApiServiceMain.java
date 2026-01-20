cbcce03ac48c5fef5de9788c29a7b7942d734c37
YuYoungKwang <db99817@gmail.com>
2026-01-20 09:20:41 +0900

    공공데이터 API JSON 파싱 (json-simple 라이브러리 추가), DTO 추가 공공데이터 API 구조개선(v1, v2), 파라미터 활용

---
+++ src/be_study/api/v1/ApiServiceMain.java
@@ -0,0 +1,18 @@
+package be_study.api.v1;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class ApiServiceMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MidFcstInfoServiceRepository repository = new MidFcstInfoServiceRepository();
+		
+		MidTaDTO midTaDTO = repository.getMidTa("202601150600", "11B10101");
+		System.out.println(midTaDTO);
+		MidTaDTO midTaDTO2 = repository.getMidTa("202601150600", "11B20201");
+		System.out.println(midTaDTO2);
+	}
+}
